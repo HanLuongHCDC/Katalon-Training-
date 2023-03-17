d20da2867a1f366ad37df537465b6e016f8f4911
Han Luong <hanluong@kms.com.vn>
2023-03-17 12:09:34 +0700

    Exercise

---
+++ Object Repository/BA4-TC001 - Verify Contact Us/Set FName in First Name box.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Set FName in First Name box</name>
+   <tag></tag>
+   <elementGuidId>b5b620e3-ed15-4dcc-9a99-55ebb20a4751</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@name = 'first_name']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
